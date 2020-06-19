236e722e7b4b822bde3916646e97770000450181
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-06-19 09:49:56 -0700

    reworked, dried up code

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// when something is entered at a time block, to save it it looks like saving an object with a pair of the time in question and the todostring
-// compare row value (hr number/24) to current time, or assign value for comparison
-// so for all of the blocks, (array corresponding)
-// I need to make a timestamp, an input, and a submit button
-// middle input needs conditional styling based on current/past/future
-// use isBefore and isAfter notation to call
-
-// textarea.val() define to var, use that var to pop
-// work with this.target and if target.class=button
-
-// running a for loop to check for filling
-
 $("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
-var currentTime = moment().format("H");
+var now = parseInt(moment().format("H"));
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var timeEl = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+// in case I need to store the stuff in an array of obj like last time 
+// var storedSched = []
 
 function generateBlocks() {
   // make time block
@@ -23,58 +13,34 @@ function generateBlocks() {
   newHour.appendTo(`#${hours[i]}`);
   // make input
   input = $("<input>");
-  input.addClass("col col-8 textarea");
-  // colorizing each
-  if (currentTime === hours[i]) {
+  input.addClass("col col-8");
+
+  // colorizing each -- had to parse the time comparison
+  if (now === hours[i]) {
     input.addClass("present");
-  } else if (currentTime > hours[i]) {
+  } else if (now > hours[i]) {
     input.addClass("past");
   } else input.addClass("future");
+
+  // turning the stored value into the showed value 
   input.text(hours[i]);
+  input.val(localStorage.getItem(`${hours[i]}`));
   input.appendTo(`#${hours[i]}`);
+
   // make button
   newBtn = $("<button>");
   newBtn.addClass("col col-2 saveBtn text-center");
   newBtn.html('<h1><i class="fas fa-save"></i></h1>');
   newBtn.appendTo(`#${hours[i]}`);
 }
-// making all the blocks and colorizing via function above
+// making all the blocks and colorizing via function above **note, colors not functioning perfectly**
 for (i = 0; i < 9; i++) {
   generateBlocks();
 }
-
+// click handler
 $("button").click(function (event) {
-  if ($(this).prev()) {
-      var thingtoAdd = {};
-      thingtoAdd["time"] = parseInt($(this).prev().text());
-      thingtoAdd["entry"] = $(this).prev().val().trim();
-      // console.log(thingtoAdd);
-      if (thingtoAdd.entry) {
-        localStorage.setItem(
-          parseInt($(this).prev().text()),
-          JSON.stringify($(this).prev().val().trim())
-        );
-      }
-    }
-    // .
-    // if the string retrieved for the number is not empty
-  });
+  var timeInput = parseInt($(this).prev().text());
+  var entryInput =  $(this).prev().val();
+  localStorage.setItem(`${timeInput}`, entryInput);
+});
 
- 
-
-// parseInt($(this).prev().text())}
-
-// function storeEntry() {
-//   if ($(this).prev()) {
-//     var thingtoAdd = {};
-//     thingtoAdd["time"] = parseInt($(this).prev().text());
-//     thingtoAdd["entry"] = $(this).prev().val().trim();
-//     console.log(thingtoAdd);
-//     if (thingtoAdd.entry) {
-//       localStorage.setItem(
-//         parseInt($(this).prev().text()),
-//         JSON.stringify($(this).prev().val().trim())
-//       );
-//     }
-//   }
-// }
